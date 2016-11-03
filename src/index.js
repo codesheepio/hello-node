163fbd7aaceb0239671a3944906d5e5bc3e1816c
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-11-03 18:45:03 +0700

    bind this variable to dog

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-const logWaitTime = (waittime) => {
-  console.log('Wait ' + waittime + 'millisecs')
-}
-const waitThenLog = (waittime) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve(waittime), waittime)
-  })
+let dog = {
+  sound: 'hong hong',
+  bark: function() {
+    console.log(this.sound)
+  }
 }
 
-/*waitThenLog(500)
-  .then((waittime) => {
-    logWaitTime(waittime)
-    return waitThenLog(300)
-  })
-  .then(waittime => {
-    logWaitTime(waittime)
-    console.log('no wait')
-  })*/
+let cat = {
+  sound: 'nian nian',
+  bark: dog.bark.bind(dog)
+}
 
-/*waitThenLog(500)
-  .then((waittime) => { return logWaitTime(waittime) })
-  .then(() => { return waitThenLog(300) })
-  .then((waittime) => { return logWaitTime(waittime) })
-  .then(() => console.log('no wait'))*/
-
-  waitThenLog(500)
-    .then(logWaitTime)
-    .then(() => { return waitThenLog(300) })
-    .then(logWaitTime)
-    .then(() => console.log('no wait'))
-
+cat.bark()
 
